a01c3faff04dfdf2b6b240376594a8dfe493d11c
Sudhanshu jha <116909414+SudhanshuDTU@users.noreply.github.com>
2024-01-06 18:20:17 +0530

    Add files via upload

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+   <div > <img width={'150px'}  className=' rounded-2xl' src={'https://i.postimg.cc/BbRtgqdY/Screenshot-2024-01-06-at-2-43-10-AM.png'} alt='main'/></div>
+  )
+}
+
+export default Logo
